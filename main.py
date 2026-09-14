cdf90e0db85939d45a858ea53a938a2930e7836a
Luana Bonagurio Pavan <l257243@LE14-2.grad.fee.unicamp.br>
2026-09-14 17:30:30 -0300

    Quantidade funcionando com o led piscando proporcionalmente

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import network
 import ntptime
 import time
 from machine import Pin
-from menu import menu_principal
-from menu import horarios
+import menu  # Importado para ler a variável quantidade_atual dinamicamente[cite: 1]
+from menu import menu_principal, horarios
 
 # LED Verde no GPIO 11 da BitDogLab V7[cite: 2]
 led = Pin(11, Pin.OUT)
@@ -41,6 +41,15 @@ def obter_horario_local():
     tm = time.localtime(time.time() + fuso_utc_3)
     return tm[3], tm[4], tm[5]
 
+def piscar_led_por_tempo(duracao_segundos):
+    """Pisca o LED Verde continuamente durante o tempo total em segundos."""
+    tempo_inicio = time.time()
+    while (time.time() - tempo_inicio) < duracao_segundos:
+        led.value(1)
+        time.sleep_ms(100)
+        led.value(0)
+        time.sleep_ms(100)
+
 ultimo_minuto_disparado = -1
 
 def verificar_alimentacao():
@@ -50,13 +59,22 @@ def verificar_alimentacao():
     if segundo == 0 and minuto != ultimo_minuto_disparado:
         for refeicao, horaf in horarios.items():
             if hora == horaf[0] and minuto == horaf[1]:
-                print(f"!!! ALIMENTANDO: {refeicao} !!!")
-                # Pisca o LED Verde para simular acionamento[cite: 1]
-                for _ in range(10):
-                    led.value(1)
-                    time.sleep_ms(100)
-                    led.value(0)
-                    time.sleep_ms(100)
+                # Lê a quantidade atual configurada no menu ("Pouco", "Medio", "Muito")[cite: 1]
+                qtd = getattr(menu, 'quantidade_atual', 'Medio')
+                
+                # Mapeia a quantidade para o tempo de piscada em segundos
+                if qtd == "Pouco":
+                    tempo_execucao = 5
+                elif qtd == "Muito":
+                    tempo_execucao = 15
+                else:  # "Medio"
+                    tempo_execucao = 10
+
+                print(f"!!! ALIMENTANDO: {refeicao} | Qtd: {qtd} | Tempo: {tempo_execucao}s !!!")
+                
+                # Executa o pisca-pisca pelo tempo determinado[cite: 1, 2]
+                piscar_led_por_tempo(tempo_execucao)
+
                 ultimo_minuto_disparado = minuto
                 break
 
